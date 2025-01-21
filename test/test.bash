30147e238d85987f695e94c712789c4e65b32cfc
Ikki Chikaraishi <23c1090ew@s.chibakoudai.jp>
2025-01-21 17:22:56 +0900

    test add

--- test/test.bash
+++ test/test.bash
@@ -2,26 +2,43 @@
 #SPDX-FileCopyrightText:2025 Ikki Chikaraishi
 #SPDX-Licence-Identifier:BSD-3-Clause
 
+res=0
+
 dir=~
 [ "$1" != "" ] && dir="$1"   #引数があったら、そちらをホームに変える.
+
+sudo apt -y install python3-pip
+pip install psutil
 
 cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
-timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
+timeout 10 ros2 launch mypkg memory_listen.launch.py &> /tmp/mypkg_test.log
 
-cat /tmp/mypkg.log | grep '2025-01-01: 元日'
+totalmem=`free -b | awk 'NR==2 {print substr($2,1)}'`
+
+cat /tmp/mypkg_test.log
+
+grep $totalmem /tmp/mypkg_test.log
 if [ $? = 0 ]; then
-    echo 1/1 OK
+    echo TotalMemoryOK
 else
-    echo 1/1 NG
+    echo TotalMemoryNG
     res=1
 fi
 
-cat /tmp/mypkg.log | grep '2025-01-03'
-if [ $? = 0 ]; then
-    echo 1/3 OK
+if grep -E 'Free_Memory: [0-9]+' /tmp/mypkg_test.log; then
+    echo FreeMemoryOK
 else
-    echo 1/3 NG
+    echo FreeMemoryNG
     res=1
 fi
+
+if grep -E 'Used_memory: [0-9]+' /tmp/mypkg_test.log; then
+    echo UsedMemoryOK
+else
+    echo UsedMemoryNG
+    res=1
+fi
+
+exit $res
